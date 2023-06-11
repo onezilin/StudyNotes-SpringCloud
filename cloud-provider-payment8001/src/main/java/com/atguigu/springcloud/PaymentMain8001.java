3c7b882e962d96ad2f57b62d0bf96c7e4dd20deb
onezilin <onezilinil@gamil.com>
2023-06-11 22:36:37 +0800

    MOD：修改 cloud-provider-payment8001 模块，调整依赖、添加主要方法

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -1,7 +1,14 @@
 package com.atguigu.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Description:
  */
+@SpringBootApplication
 public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
 }
