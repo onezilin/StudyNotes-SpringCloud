458c083af56d4a5b0f4ec7fec7e7454628889a72
onezilin <onezilinil@gamil.com>
2023-06-24 19:52:39 +0800

    MOD：order80 注册进 eureka

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -2,11 +2,14 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Description:
  */
 @SpringBootApplication
+// 声明自己是 eureka 客户端
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
