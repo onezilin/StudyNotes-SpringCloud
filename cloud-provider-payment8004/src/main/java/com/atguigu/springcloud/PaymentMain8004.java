4e8453d16159304e2d79b58b4b541722ec23a7f3
onezilin <onezilinil@gamil.com>
2023-07-03 00:59:52 +0800

    ADD：添加 cloud-provider-payment8004 模块，使用 Zookeeper 服务注册发现中心（注意 pom.xml 中 Zookeeper 依赖版本）

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Description:
+ */
+@SpringBootApplication
+// 启动服务注册发现功能，向服务注册中心注册服务
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
