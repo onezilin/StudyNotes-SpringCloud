120490407dfac1d8aa1606b65ebe8f816f981370
onezilin <onezilinil@gamil.com>
2023-07-02 16:35:18 +0800

    MOD：服务发现 DiscoveryClient

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -3,13 +3,17 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * (Payment)表控制层
@@ -17,6 +21,7 @@ import javax.annotation.Resource;
  * @author onezilin
  * @since 2023-06-04 19:20:45
  */
+@Slf4j
 @RestController
 @RequestMapping("/payment")
 public class PaymentController {
@@ -25,6 +30,9 @@ public class PaymentController {
      */
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoryClient;
 
     @Value("${server.port}")
     private String serverPort;
@@ -85,5 +93,20 @@ public class PaymentController {
         return ResponseEntity.ok(this.paymentService.deleteById(id));
     }
 
+    @GetMapping("/discovery")
+    public DiscoveryClient discovery() {
+        // 获取注册的微服务名称，也就是 spring.application.name
+        List<String> serviceNameList = discoryClient.getServices();
+
+        serviceNameList.forEach(serviceName -> {
+            // 获取微服务名称具体的主机名称、ip、端口等信息
+            List<ServiceInstance> serviceInstancesList = discoryClient.getInstances(serviceName);
+            serviceInstancesList.forEach(serviceInstance -> {
+                log.info("服务名：{}，主机名：{}，端口：{}，URL地址：{}", serviceInstance.getServiceId(), serviceInstance.getHost(),
+                        serviceInstance.getPort(), serviceInstance.getUri());
+            });
+        });
+        return discoryClient;
+    }
 }
 
--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,6 +11,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 // 声明自己是 eureka 客户端
 @EnableEurekaClient
+// 开启服务发现功能
+@EnableDiscoveryClient
 public class PaymentMain8002 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8002.class, args);
