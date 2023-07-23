0d85d821f7589644c35810291ed9bed1c506de3a
onezilin <onezilinil@gamil.com>
2023-07-23 16:49:45 +0800

    MOD：getForEntity()方法使用

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,6 +25,24 @@ public class OrderController {
     @GetMapping("/payment/get/{id}")
     public CommonResult<Payment> get(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    /**
+     * Description: getForEntity 包含请求头、响应体（也就是 getForObject 的返回值）、响应状态码等信息
+     *
+     * @param id id
+     * @return com.atguigu.springcloud.entities.CommonResult<com.atguigu.springcloud.entities.Payment>
+     */
+    @GetMapping("/payment/getForEntity/{id}")
+    public CommonResult<Payment> get2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id,
+                CommonResult.class);
+
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444, "操作失败");
+        }
     }
 
     @GetMapping("/payment/create")
