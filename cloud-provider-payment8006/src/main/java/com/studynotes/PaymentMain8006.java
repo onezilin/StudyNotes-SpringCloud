90b73e2f0e20fa54cc743f6e1c266398c39e510e
onezilin <onezilinil@gamil.com>
2023-07-23 11:36:26 +0800

    ADD：添加 cloud-provider-payment8006 模块，使用 Consul 服务注册发现中心

---
+++ cloud-provider-payment8006/src/main/java/com/studynotes/PaymentMain8006.java
@@ -0,0 +1,14 @@
+package com.studynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+// 启动服务注册发现功能，向服务注册中心注册服务
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class, args);
+    }
+}
