4dad7e6331784d24c08038a6c98e39756bd6858c
onezilin <onezilinil@gamil.com>
2023-08-09 17:51:08 +0800

    MOD：负载均衡的动态路由配置

--- cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GatewayMain9527.java
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GatewayMain9527.java
@@ -3,9 +3,12 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+// 指定自定义的负载均衡算法
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = com.atguigu.myrule.MySelfRule.class)
 public class GatewayMain9527 {
 
     public static void main(String[] args) {
