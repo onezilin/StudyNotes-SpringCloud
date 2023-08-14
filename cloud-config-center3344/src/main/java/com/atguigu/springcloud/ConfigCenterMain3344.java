0856c8ab5520e8b4c6fa821ffc712016789eb128
onezilin <onezilinil@gamil.com>
2023-08-14 14:22:49 +0800

    ADD：添加 cloud-config-center3344 模块

---
+++ cloud-config-center3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
