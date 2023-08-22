c5d1bf080c80d70737da48747410aad6579eea8c
onezilin <onezilinil@gamil.com>
2023-08-22 11:04:42 +0800

    MOD：添加手动刷新配置的功能（注意添加 Actuator 依赖，使用 POST 请求 http://地址:端口/actuator/refresh 刷新指定服务配置）

--- cloud-config-client3366/src/main/java/com/atguigu/springcloud/controller/ConfigController.java
+++ cloud-config-client3366/src/main/java/com/atguigu/springcloud/controller/ConfigController.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Description:
  */
 @RestController
+@RefreshScope // 支持 Config、Nacos 的动态刷新功能
 public class ConfigController {
 
     @Value("${config.info}")
