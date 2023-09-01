0c373bafecfe98a8c90725c4ad62713dcf2a4692
onezilin <onezilinil@gamil.com>
2023-09-01 14:46:01 +0800

    ADD：添加 cloudalibaba-config-nacos-client3377（已指定 namespace、group、dataId）

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/ConfigNacosClientMain3377.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigNacosClientMain3377 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigNacosClientMain3377.class, args);
+    }
+}
