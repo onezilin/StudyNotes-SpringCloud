0d1b8d847d82352555bd7060a96496f372e2dd57
onezilin <onezilinil@gamil.com>
2023-10-07 16:28:52 +0800

    ADD：添加 cloudalibaba-sentinel-service8401 模块（注意开启 Sentinel 控制台）

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/FlowLimiterController.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description:
+ */
+@RestController
+@RequestMapping("/flowRule")
+public class FlowLimiterController {
+
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "------testB";
+    }
+}
