ae96b08d1d7c8d416f1dc9fe3adcb046c8cbad67
onezilin <onezilinil@gamil.com>
2023-10-25 11:03:45 +0800

    ADD：添加 cloudalibaba-sentinel-service8402 模块（用于测试集群流量控制，将 sentinel-core 版本改为 1.8.0 防止依赖出问题）

---
+++ cloudalibaba-sentinel-service8402/src/main/java/com/atguigu/springcloud/SentinelMain8402.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelMain8402 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelMain8402.class, args);
+    }
+}
