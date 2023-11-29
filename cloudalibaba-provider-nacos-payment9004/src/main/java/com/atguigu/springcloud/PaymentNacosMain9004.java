a3bd5cb8c99b6f22a40a37e1a2703e189a0f5db9
wanzilin <wanzilin@kingtree.com.cn>
2023-11-29 14:01:48 +0800

    ADD：添加 cloudalibaba-provider-nacos-payment9003 和 cloudalibaba-provider-nacos-payment9004 模块（使用 Nacos 作为服务注册发现中心）

---
+++ cloudalibaba-provider-nacos-payment9004/src/main/java/com/atguigu/springcloud/PaymentNacosMain9004.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentNacosMain9004 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentNacosMain9004.class, args);
+    }
+}
