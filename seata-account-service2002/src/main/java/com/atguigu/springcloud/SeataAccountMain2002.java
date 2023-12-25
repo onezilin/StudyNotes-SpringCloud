cd67d27d21d4b32aabc6685cca3087fcf87eb235
onezilin <onezilinil@gmail.com>
2023-12-25 17:41:23 +0800

    MOD：【XA 模式】——指定数据源代理为 XA 模式

--- seata-account-service2002/src/main/java/com/atguigu/springcloud/SeataAccountMain2002.java
+++ seata-account-service2002/src/main/java/com/atguigu/springcloud/SeataAccountMain2002.java
@@ -12,8 +12,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @MapperScan({"com.atguigu.springcloud.dao"})
-// 开启 Seata 对数据源的代理
-@EnableAutoDataSourceProxy
+// 开启 Seata 对数据源的代理，指定数据源代理为 XA 模式
+// 测试步骤：调用 business 接口，查看 undo_log 表，如果没有 before image、after image 等中间数据，则说明为 XA 模式
+@EnableAutoDataSourceProxy(dataSourceProxyMode = "XA")
 public class SeataAccountMain2002 {
 
     public static void main(String[] args) {
